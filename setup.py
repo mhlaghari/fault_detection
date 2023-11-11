b68fd23b815ae1ef3c15af6997279b06595ae056
mhlaghari <mhlaghari@gmail.com>
2023-11-12 02:38:53 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='sensor',
+    version='0.0.1',
+    author='Hamza Laghari',
+    author_email='mhlaghari@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements(),
+    
+)
