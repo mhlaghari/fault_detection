54765909b1df4a2e0e2712649aa13cb63d97d5b6
mhlaghari <mhlaghari@gmail.com>
2023-11-16 00:12:27 +0530

    updated exception and logging

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
-import pymongo
+from sensor.utils import get_collection_as_dataframe
+import os, sys
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
-
-# Database Name
-dataBase = client["neurolabDB"]
-
-# Collection  Name
-collection = dataBase['Products']
-
-# Sample data
-d = {'companyName': 'iNeuron',
-     'product': 'Affordable AI',
-     'courseOffered': 'Machine Learning with Deployment'}
-
-# Insert above records in the collection
-rec = collection.insert_one(d)
-
-# Lets Verify all the record at once present in the record with all the fields
-all_record = collection.find()
-
-# Printing all records present in the collection
-for idx, record in enumerate(all_record):
-     print(f"{idx}: {record}")
-#https://raw.githubusercontent.com/avnyadav/sensor-fault-detection/main/aps_failure_training_set1.csv
+if __name__ == "__main__":
+     try:
+          get_collection_as_dataframe(database_name='aps', collection_name='sensor')
+     except Exception as e:
+          print(e)
+     
